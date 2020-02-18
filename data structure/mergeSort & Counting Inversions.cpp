f18ad626c0bdcbea62534e59e7c68266264b36e4
inju2403 <inju2403@gmail.com>
2020-02-18 18:24:21 +0900

    none

--- data structure/mergeSort & Counting Inversions.cpp
+++ data structure/mergeSort & Counting Inversions.cpp
@@ -1,21 +1,27 @@
+long long res;
+vector<int> a, tmp;
+
 void merge(int l,int mid,int r) {
 	int i=l;
-	int j=r;
+	int j=mid+1;
 	int k=l;
+	
 	while(i<=mid && j<=r) {
-		if(a[i]<a[j]) {
-			tmp[k++]=a[i++];
+		if(a[i]<=a[j]) {
+			tmp[k++]=a[i++];	
 		}
 		else {
 			tmp[k++]=a[j++];
-			rank[]-=(mid-i+1);
+			res+=mid-(i-1);	   // swap이 일어날 횟수
 		}
 	}
-	while(j<=r) tmp[k++]=a[j++];
+	
+    while(j<=r) tmp[k++]=a[j++];
 	while(i<=mid) tmp[k++]=a[i++];
 	
-	FORS(idx,l,r+1) a[idx]=tmp[idx];
+	for(int idx=l; idx<=r; ++idx) a[idx]=tmp[idx];
 }
+
 
 void mergeSort(int l,int r) {
 	if(l<r) {
