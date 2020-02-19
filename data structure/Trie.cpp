c136de13309ca8dfb3edbe60e42b2f16643257f3
inju2403 <inju2403@gmail.com>
2020-02-19 22:48:14 +0900

    none

---
+++ data structure/Trie.cpp
@@ -0,0 +1,24 @@
+struct Trie {
+	bool finish;
+	Trie* children[26];
+	Trie() : finish(false) {
+		memset(children,0,sizeof(children));
+	}
+	~Trie() {
+		FOR(i,26) if(children[i]) delete children[i];
+	}
+	void insert(const char* key) {
+		if(*key==0) finish=true;
+		else {
+			int next=*key-'A';
+			if(children[next]==NULL) children[next]=new Trie();
+			children[next]->insert(key+1);
+		}
+	}
+	Trie* find(const char* key) {
+		if(*key==0) return this;
+		int next=*key-'A';
+		if(children[next]==NULL) return NULL;
+		children[next]->insert(key+1);
+	}
+};
